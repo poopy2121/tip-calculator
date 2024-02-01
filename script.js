46dcae42a65e030e5e9ebe27b0159819dfb0339f
poopy2121 <ggg1111g1g1g@gmail.com>
2024-02-01 23:35:52 +0100

    keep going

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const calculatebtn = document.getElementById("calculate")
+let result = document.getElementById("result")
+
+
+calculatebtn.addEventListener('click', function calculation() {
+    
+    let tipamount = document.getElementById("tipamount").value
+    let formula = ()
+
+
+
+
+
+    
+})
